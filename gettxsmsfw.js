c579165e4bc7dcd88359a8e26758b9ecf2e19bb2
akinachann <76243283+akinachann@users.noreply.github.com>
2026-03-24 23:50:25 +0800

    Update gettxsmsfw.js

--- gettxsmsfw.js
+++ gettxsmsfw.js
@@ -15,6 +15,12 @@ try {
             case '企业微信':
                 notify = '2';
                 break;
+            case 'Telegram':
+                notify = '3';
+                break;
+            case '飞书':
+                notify = '4';
+                break;
             default:
                 notify = '-1';
         }
@@ -47,7 +53,6 @@ function main() {
         forward = new RegExp(regexstr, 'i').test(sms);
     }
 
-    //使用switch语句,预留自定义通知方式,如有需要,自行参考对应的API文档编写代码
     if (forward) {
         switch (notify) {
             case '0':
@@ -58,6 +63,12 @@ function main() {
                 break;
             case '2':
                 wxnotification(tokenkey, [smsender, sms]);
+                break;
+            case '3':
+                tgnotification(tokenkey, [smsender, sms]);
+                break;
+            case '4':
+                feishunotification(tokenkey, [smsender, sms]);
                 break;
             default:
                 $notification.post($script.name, '', '参数不正确,停止运行');
@@ -99,4 +110,30 @@ function wxnotification(sendkey, smsender) {
     const wxwebhookurl = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=' + sendkey;
     const reqbody = `{"msgtype":"text","text":{"content":"发送号码:${smsender[0]} 短信内容:${smsender[1]}"}}`
     postmsg(wxwebhookurl, reqbody);
+}
+
+// Telegram Bot API
+// tokenkey 格式: BotToken.ChatID
+// BotToken 形如 123456789:AAFxxxxxxx,ChatID 形如 -100123456789
+// 完整填写示例: 123456789:AAFxxxxxxx.-100123456789
+// 脚本以最后一个 . 为分隔,前段为 BotToken,后段为 ChatID
+function tgnotification(tokenkey, smsender) {
+    const lastDot = tokenkey.lastIndexOf('.');
+    const bottoken = tokenkey.substring(0, lastDot).trim();
+    const chatid = tokenkey.substring(lastDot + 1).trim();
+    const tgurl = `https://api.telegram.org/bot${bottoken}/sendMessage`;
+    const text = `📱 TX短信转发\n发送号码: ${smsender[0]}\n短信内容: ${smsender[1]}`;
+    const reqbody = JSON.stringify({ chat_id: chatid, text: text });
+    postmsg(tgurl, reqbody);
+}
+
+// 飞书自定义机器人 Webhook
+// tokenkey 格式: 填写 webhook 地址末尾的 hook token
+// 即 https://open.feishu.cn/open-apis/bot/v2/hook/{此处} 的值
+// 注意: 若飞书机器人开启了签名校验,请改用关键词校验,在消息关键词中加入"TX短信"即可
+function feishunotification(hooktoken, smsender) {
+    const feishuurl = `https://open.feishu.cn/open-apis/bot/v2/hook/${hooktoken}`;
+    const text = `TX短信转发\n发送号码: ${smsender[0]}\n短信内容: ${smsender[1]}`;
+    const reqbody = JSON.stringify({ msg_type: 'text', content: { text: text } });
+    postmsg(feishuurl, reqbody);
 }
